094ab3de7045894c9eae04df5e54e8aeb619c106
Daniel Lucas <danielcerqueira@live.com>
2024-06-03 02:16:55 -0300

    final update bcc 1.5

--- R/bcc.R
+++ R/bcc.R
@@ -1,6 +1,6 @@
 #' Beta Control Charts
 #'
-#' The Beta Chart presents control limits based on the Beta probability distribution. It is used for monitoring fraction data from a Binomial distribution as a replacement for p-Charts. The Beta Chart has been applied in three real studies, demonstrating its effectiveness. Comparative analysis revealed that: (i) the Beta approximation to the Binomial distribution is more appropriate for values confined within the [0, 1] interval; and (ii) the proposed charts are more sensitive to the average run length (ARL) in both in-control and out-of-control process monitoring. Overall, the Beta Charts outperform the Shewhart control charts for monitoring fraction data.
+#' The Beta Chart presents control limits based on the Beta probability distribution. It is used for monitoring fraction data from a Binomial distribution as a replacement for p-Charts. The Beta Chart has been applied in three real studies, demonstrating its effectiveness. Comparative analysis revealed that: (i) the Beta approximation to the Binomial distribution is more appropriate for values confined within the \code{[0, 1]} interval; and (ii) the proposed charts are more sensitive to the average run length (ARL) in both in-control and out-of-control process monitoring. Overall, the Beta Charts outperform the Shewhart control charts for monitoring fraction data.
 #'
 #' @aliases bcc
 #' @description Creates and displays a Beta control chart using the specified data, sample sizes, and type. This is the main function for generating control charts in this package.
@@ -14,26 +14,19 @@
 #'
 #' @examples
 #' # Example for type 1 chart with discrete data
-#' data <- c(0.1, 0.2, 0.15, 0.3, 0.25)
-#' sizes <- c(100, 100, 100, 100, 100)
+#' data <- c(0.12, 0.18, 0.14, 0.28, 0.22)
+#' sizes <- c(101, 98, 110, 105, 95)
 #' bcc(data, sizes, type=1, title="Beta Control Chart for Discrete Data")
 #'
 #' # Example for type 2 chart with continuous data
-#' data <- c(0.55, 0.60, 0.65, 0.70, 0.75)
+#' data <- c(0.59, 0.67, 0.61, 0.70, 0.59)
 #' bcc(data, type=2, title="Beta Control Chart for Continuous Data")
 #'
 #' # Example changing the title of the chart
-#' data <- c(0.05, 0.10, 0.15, 0.20, 0.25)
-#' sizes <- c(50, 50, 50, 50, 50)
+#' data <- c(0.07, 0.13, 0.21, 0.25, 0.19)
+#' sizes <- c(52, 49, 51, 53, 48)
 #' bcc(data, sizes, type=1, title="Custom Title: Beta Control Chart for Discrete Data")
 #'
-#' # Example with uses Montgomery's data
-#' data_values <- Montgomery2005$Defective
-#' sample_sizes <- Montgomery2005$Sample
-#' limits <- calculate_limits_type1(Montgomery2005$Defective, Montgomery2005$Sample)
-#' plot_control_chart(data_values, sample_sizes, type=1, limits, "Sample Control Chart")
-
-
 bcc <- function(data, sizes=NULL, type, title=NULL) {
   # If no title is provided, create a default title using the name of the data variable
   if (is.null(title)) {
